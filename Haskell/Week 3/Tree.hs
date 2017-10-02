727b9f195904a3faad94fece12a349a38a27aa34
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-10-02 19:14:41 +0200

    Added material to Tree.hs

--- Haskell/Week 3/Tree.hs
+++ Haskell/Week 3/Tree.hs
@@ -24,6 +24,29 @@ where
    fr (\elt acc -> acc ++ [elt]) "" "Reversing a string" --"gnirts a gnisreveR"
    fr (\elt acc -> elt:acc) "" "Reversing a string" --"Reversing a string"
 
+
+let treeDepth :: Tree -> Int
+treeDepth Leaf = 0
+treeDepth (Node _ leftSubtree rightSubtree) =
+   1 + max (treeDepth leftSubtree) (treeDepth rightSubtree)
+
+let treeSum :: Tree -> Int
+     treeSum Leaf = 0
+     treeSum (Node value leftSubtree rightSubtree) =
+       value + (treeSum leftSubtree) + (treeSum rightSubtree)
+
+let isSortedTree :: Tree -> Int -> Int -> Bool
+     isSortedTree Leaf _ _ = True
+     isSortedTree (Node x leftSubtree rightSubtree) minVal maxVal =
+       let leftSorted   = isSortedTree leftSubtree minVal x
+           rightSorted = isSortedTree rightSubtree x maxVal
+       in x >= minVal && x< maxVal && leftSorted && rightSorted
+
+let addNewMax :: Tree -> Tree
+     addNewMax Leaf = Node 0 Leaf Leaf
+     addNewMax (Node x t1 Leaf) = Node x t1 (Node (x+1) Leaf Leaf)
+     addNewMax (Node x t1 t2) = Node x t1 (addNewMax t2)
+
 -- test submission
 data Tree a = Leaf | Node a (Tree a) (Tree a) deriving Show
 
