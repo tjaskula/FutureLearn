79a40aba1de41fcf2ac590708d62c19cc0fcd816
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-10-18 00:16:16 +0200

    First unit test on show PersonRecord

--- Haskell/Week4/ParsecTutorial/test/ShowParserSpec.hs
+++ Haskell/Week4/ParsecTutorial/test/ShowParserSpec.hs
@@ -19,14 +19,18 @@ data Address = MkAddress {
 
 data Label = Green | Red | Blue | Yellow deriving (Show)
 
+rec1 = MkPersonRecord 
+  "Wim Vanderbauwhede" 
+  (MkAddress "School of Computing Science" 17 "Lilybank Gdns" "Glasgow" "G12 8QQ")
+  557188
+  [Green, Red]
+
 spec :: Spec
 spec = 
   describe "Parsec" $ do
-    context "context" $
-      it "should be true" $
-        True `shouldBe` True
-      --it "should be false" $
-        --False `shouldBe` False
+    context "show PersonRecord 1" $
+      it "should equal to string representation" $
+        show rec1 `shouldBe` "MkPersonRecord {name = \"Wim Vanderbauwhede\", address = MkAddress {line1 = \"School of Computing Science\", number = 17, street = \"Lilybank Gdns\", town = \"Glasgow\", postcode = \"G12 8QQ\"}, id = 557188, labels = [Green,Red]}"
 
 main :: IO ()
 main = hspec spec
