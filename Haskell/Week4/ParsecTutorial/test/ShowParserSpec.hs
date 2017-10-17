ddccb63c5683eefe88dde0293bb82561dc7e1641
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-10-18 00:21:15 +0200

    Adding second unit test on show

--- Haskell/Week4/ParsecTutorial/test/ShowParserSpec.hs
+++ Haskell/Week4/ParsecTutorial/test/ShowParserSpec.hs
@@ -25,12 +25,22 @@ rec1 = MkPersonRecord
   557188
   [Green, Red]
 
+rec2 = MkPersonRecord 
+  "Jeremy Singer" 
+  (MkAddress "School of Computing Science" 17 "Lilybank Gdns" "Glasgow" "G12 8QQ")
+  42
+  [Blue, Yellow]
+
 spec :: Spec
 spec = 
   describe "Parsec" $ do
-    context "show PersonRecord 1" $
+    context "show PersonRecord Wim" $
       it "should equal to string representation" $
         show rec1 `shouldBe` "MkPersonRecord {name = \"Wim Vanderbauwhede\", address = MkAddress {line1 = \"School of Computing Science\", number = 17, street = \"Lilybank Gdns\", town = \"Glasgow\", postcode = \"G12 8QQ\"}, id = 557188, labels = [Green,Red]}"
+
+    context "show PersonRecord Jeremy" $
+      it "should equal to string representation" $
+        show rec2 `shouldBe` "MkPersonRecord {name = \"Jeremy Singer\", address = MkAddress {line1 = \"School of Computing Science\", number = 17, street = \"Lilybank Gdns\", town = \"Glasgow\", postcode = \"G12 8QQ\"}, id = 42, labels = [Blue,Yellow]}"
 
 main :: IO ()
 main = hspec spec
